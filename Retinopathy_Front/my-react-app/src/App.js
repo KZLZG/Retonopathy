72756c2e23a4f776d51131a2a4382a50085278af
KZLZG <Egor23102002@mail.ru>
2024-05-01 13:31:26 +0300

    made it beautiful

--- Retinopathy_Front/my-react-app/src/App.js
+++ Retinopathy_Front/my-react-app/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Container, Row, Col, Form, Button, Image, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [file, setFile] = useState(null);
   const [imageUrl, setImageUrl] = useState('');
+  const [imageName, setImageName] = useState('');
 
   const onFileChange = event => {
     setFile(event.target.files[0]);
+    setImageName(event.target.files[0].name);
   };
 
   const onFileUpload = () => {
@@ -21,13 +25,41 @@ function App() {
   };
 
   return (
-    <div>
-      <input type="file" onChange={onFileChange} />
-      <button onClick={onFileUpload}>
-        Upload!
-      </button>
-      {imageUrl && <img src={imageUrl} alt="Processed Image" />}
-    </div>
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand>Retinopathy</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container style={{ marginTop: '20px' }}>
+        <Row className="justify-content-md-center">
+          <Col md={6}>
+            <Form>
+              <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Выберите фото для разметки</Form.Label>
+                <Form.Control type="file" onChange={onFileChange} />
+              </Form.Group>
+              <Button variant="primary" onClick={onFileUpload}>
+                Загрузить!
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center" style={{ marginTop: '20px' }}>
+          <Col md={6} className="text-center">
+            {imageUrl && <>
+              <Image src={imageUrl} alt="Processed Image" rounded />
+              <br />
+              <a href={imageUrl} download={imageName || "downloaded_image"}>
+                <Button variant="success" style={{ marginTop: '10px' }}>
+                  Сохранить фото
+                </Button>
+              </a>
+            </>}
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
